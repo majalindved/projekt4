3d03c39a2631734324b9d8308b83df84179e9820
turkaka <turkaka33@hotmail.com>
2020-03-06 12:17:56 +0100

    kasim

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var btnContainer = document.getElementById("produkt-button");
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
-
-// Loop through the buttons and add the active class to the current/clicked button
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
   });
 }
+
+
+document.getElementById("arrangement").onclick = function () {
+    location.href = "arrangementer.html";
+};
+
+document.getElementById("arrangement1").onclick = function () {
+    location.href = "arrangementer.html";
+};
