d062302d2f9cf8f7a0733e600850412f15c00bbb
turkaka <turkaka33@hotmail.com>
2020-03-06 11:20:47 +0100

    kk

--- jquery.js
+++
@@ -1,8 +0,0 @@
-// Add active class to the current button (highlight it)
-
-$(document).ready(function(){
-$('button').click(function(){
-$('button').removeClass("active");
-$(this).addClass("active");
-});
-});
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var header = document.getElementById("buttonid");
-var btns = header.getElementsByClassName("btn");
+var btnContainer = document.getElementById("produkt-button");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
 }
