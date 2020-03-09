3f5e0db9125fda2252c74136eb87ee35398c1a8e
turkaka <turkaka33@hotmail.com>
2020-03-09 23:38:44 +0100

    menu with color

--- script.js
+++ script.js
@@ -10,10 +10,14 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 
-document.getElementById("arrangement").onclick = function () {
-    location.href = "arrangementer.html";
-};
 
-document.getElementById("arrangement1").onclick = function () {
-    location.href = "arrangementer.html";
-};
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myDIVmenu");
+var btns = header.getElementsByClassName("menu-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
